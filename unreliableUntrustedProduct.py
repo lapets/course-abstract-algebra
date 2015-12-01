6efe52bab1477f41972259f23dbc7ba0ddec44a4
Andrei Lapets <lapets@gmail.com>
2015-11-30 23:37:48 -0500

    Add POST version of server call.

--- unreliableUntrustedProduct.py
+++ unreliableUntrustedProduct.py
@@ -7,8 +7,18 @@
 ##
 ##
 
+from urllib.request import urlopen
+from urllib.parse import urlencode
+
 def unreliableUntrustedProduct(xs, n):
     url = 'http://cs-people.bu.edu/lapets/235/unreliable.php'
     return int(urlopen(url+"?n="+str(n)+"&data="+",".join([str(x) for x in xs])).read().decode())
-    
+
+def unreliableUntrustedProductPOST(xs, n):
+    url = 'http://cs-people.bu.edu/lapets/235/unreliable.php'
+    data = {'n':str(n), 'data':",".join([str(x) for x in xs])}
+    post = bytes(urllib.parse.urlencode(data).encode())
+    handler = urllib.request.urlopen('http://cs-people.bu.edu/lapets/235/unreliable.php', post)
+    return INT(handler.read().decode('utf-8'))
+
 #eof
