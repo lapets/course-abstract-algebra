0c4368f556020a733ede5af0d8d9b804390d88f6
Andrei Lapets <a@lapets.io>
2016-08-12 01:46:26 -0400

    Update README; include latest version of Sheaf library; and add HTML build script.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # course-abstract-algebra
-Lecture notes, assignments, and other materials for a one-semester course on topics in number theory, modular arithmetic, abstract algebra, and computer science applications thereof.
+
+Lecture notes, assignments, and other materials for a one-semester introductory course on topics in number theory, modular arithmetic, abstract algebra, and computer science applications thereof.
+
+PHP 5.4 or later and [Sheaf](http://sheaf.io) are required. The latest version of Sheaf is already included in this repository.
+
+It is possible to build the HTML version of the notes as follows:
+
+    php build.php
---
+++ build.php
@@ -0,0 +1,29 @@
+<?php /*********************************************************
+**
+** Script to build notes as HTML using Sheaf.
+**
+** build.php
+**
+*/
+
+////////////////////////////////////////////////////////////////
+//
+
+// Load the library and rendering hooks.
+include("sheaf/sheaf.php");
+include("sheaf/hooks/math.php");
+include("sheaf/hooks/Python.php");
+
+$s = new Sheaf(
+       array(
+           'file' => 'notes.xml',
+           'path' => 'sheaf/',
+           'message' => '<h1>Topics in Abstract Algebra for Computer Science</h1>',
+           'toc' => 'true'
+         )
+      );
+
+// Render the notes as HTML.
+@file_put_contents('index.html', $s->html());
+
+/*eof*/?>
